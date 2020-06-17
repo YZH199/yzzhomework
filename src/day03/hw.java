d20af4df5102998b67bb029a31f26664f49c2d56
YZH199 <1843295568@qq.com>
2020-06-17 15:11:58 +0800

    杨忠泽第一次作业

--- src/day03/hw.java
+++ src/day03/hw.java
@@ -9,5 +9,15 @@ public class hw {
             }
             System.out.println(" ");
         }
+        int m=5;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=m-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+           System.out.println(" ");
+        }
     }
 }
