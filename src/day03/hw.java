17169144922e9c6756052a3c96bee3350d39042e
YZH199 <1843295568@qq.com>
2020-06-17 10:50:19 +0800

    杨忠泽第一次作业

--- src/day03/hw.java
+++ src/day03/hw.java
@@ -2,6 +2,12 @@ package day03;
 
 public class hw {
     public static void main(String[] args) {
-        System.out.println("cg");
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=i;j++){
+                int m=i*j;
+                System.out.print(j+" * "+i+" = "+m+" ");
+            }
+            System.out.println(" ");
+        }
     }
 }
